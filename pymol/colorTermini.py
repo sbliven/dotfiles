8f23e138fa215962efab5d790934a935566f58b2
blivens <blivens@85a3395b-4483-483f-9d08-cca67419bac5>
2010-03-11 23:53:53 +0000

    Added selectTermini function + variants
    git-svn-id: https://quantum7.svn.beanstalkapp.com/general/config@99 85a3395b-4483-483f-9d08-cca67419bac5

--- pymol/colorTermini.py
+++ pymol/colorTermini.py
@@ -1,24 +1,42 @@
 from pymol import cmd, stored
 import sys
 
-def getTermini(selection):
+def getNterm(selection):
     stored.nterm = dict()
-    stored.cterm = dict()
     obj="\"sele\"" #TODO iterate through all objects in selection
     command = "if int(resi) < stored.nterm.get((%s,chain),sys.maxint):\n" \
         "\tstored.nterm[(%s,chain)]=int(resi);\n" %(obj,obj)
     cmd.iterate(selection+" and n. ca and elem c", command)
+    nterms= "(%s) and (%s)" %(selection,
+            " or ".join( \
+                ["(chain %s and resi %d)"%(chain, res) for ((obj,chain),res) in stored.nterm.items()] ) )
+    return nterms
+
+def getCterm(selection):
+    stored.cterm = dict()
+    obj="\"sele\"" #TODO iterate through all objects in selection
     command = "if int(resi) > stored.cterm.get((%s,chain),-1):\n" \
         "\tstored.cterm[(%s,chain)]=int(resi)\n" %(obj,obj)
     cmd.iterate(selection+" and n. ca and elem c", command)
-    return (stored.nterm, stored.cterm)
-if sys.version_info > (2,5,0,'',0):
-    cmd.extend("getTermini",lambda sele: sys.stdout.write("%s %s"%getTermini(sele)))
+    cterms= "(%s) and (%s)" %(selection,
+            " or ".join( \
+                ["(chain %s and resi %d)"%(chain, res) for ((obj,chain),res) in stored.cterm.items()] ) )
+    return cterms
 
-def colorTermini(selection):
-    (nterms, cterms) = getTermini(selection)
-    for chainID in nterms.keys():
-        cmd.color("blue","%s and resi %d"%(selection,nterms[chainID]))
-        cmd.color("red","%s and resi %d"%(selection,cterms[chainID]))
-        cmd.show("spheres","%s and resi %d+%d and n. ca"%(selection,nterms[chainID],cterms[chainID]))
+def selectNterm(selection="*",name="sele"):
+    cmd.select(name,getNterm(selection))
+cmd.extend("selectNterm",selectNterm)
+def selectCterm(selection="*",name="sele"):
+    cmd.select(name,getCterm(selection))
+cmd.extend("selectCterm",selectCterm)
+def selectTermini(selection="*",name="sele"):
+    cmd.select(name,"(%s) or (%s)"%(getNterm(selection), getCterm(selection)) )
+cmd.extend("selectTermini",selectTermini)
+
+def colorTermini(selection="*"):
+    nterms = getNterm(selection)
+    cterms = getCterm(selection)
+    cmd.color("blue",nterms)
+    cmd.color("red",cterms)
+    cmd.show("spheres","((%s) or (%s)) and n. ca"%(nterms,cterms))
 cmd.extend("colorTermini",colorTermini)
