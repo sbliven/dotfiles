6bbaf81ccac4ad51af56df45e2843322b44ab124
blivens <blivens@85a3395b-4483-483f-9d08-cca67419bac5>
2010-03-11 22:57:17 +0000

    Added spheres to the ends Now deals with chains properly when colorTermini is called on a single object. If called on two objects, assumes chain ids are unique
    git-svn-id: https://quantum7.svn.beanstalkapp.com/general/config@98 85a3395b-4483-483f-9d08-cca67419bac5

--- pymol/colorTermini.py
+++ pymol/colorTermini.py
@@ -2,20 +2,23 @@ from pymol import cmd, stored
 import sys
 
 def getTermini(selection):
-    stored.nterm = sys.maxint
-    stored.cterm = -1
-    command = "if int(resi) < stored.nterm:\n" \
-        "\tstored.nterm=int(resi);\n"
+    stored.nterm = dict()
+    stored.cterm = dict()
+    obj="\"sele\"" #TODO iterate through all objects in selection
+    command = "if int(resi) < stored.nterm.get((%s,chain),sys.maxint):\n" \
+        "\tstored.nterm[(%s,chain)]=int(resi);\n" %(obj,obj)
     cmd.iterate(selection+" and n. ca and elem c", command)
-    command = "if int(resi) > stored.cterm:\n" \
-        "\tstored.cterm=int(resi)\n"
+    command = "if int(resi) > stored.cterm.get((%s,chain),-1):\n" \
+        "\tstored.cterm[(%s,chain)]=int(resi)\n" %(obj,obj)
     cmd.iterate(selection+" and n. ca and elem c", command)
     return (stored.nterm, stored.cterm)
 if sys.version_info > (2,5,0,'',0):
     cmd.extend("getTermini",lambda sele: sys.stdout.write("%s %s"%getTermini(sele)))
 
 def colorTermini(selection):
-    (nterm, cterm) = getTermini(selection)
-    cmd.color("blue","%s and resi %d"%(selection,nterm))
-    cmd.color("red","%s and resi %d"%(selection,cterm))
+    (nterms, cterms) = getTermini(selection)
+    for chainID in nterms.keys():
+        cmd.color("blue","%s and resi %d"%(selection,nterms[chainID]))
+        cmd.color("red","%s and resi %d"%(selection,cterms[chainID]))
+        cmd.show("spheres","%s and resi %d+%d and n. ca"%(selection,nterms[chainID],cterms[chainID]))
 cmd.extend("colorTermini",colorTermini)
