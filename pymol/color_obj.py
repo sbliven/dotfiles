a127d66e81e6ce71043f8c236b3ac5057c5b4eb6
blivens <blivens@85a3395b-4483-483f-9d08-cca67419bac5>
2011-12-23 20:49:44 +0000

    Merged latest changes from pymol wiki
    git-svn-id: https://quantum7.svn.beanstalkapp.com/general/config@106 85a3395b-4483-483f-9d08-cca67419bac5

--- pymol/color_obj.py
+++ pymol/color_obj.py
@@ -2,10 +2,13 @@
 #
 # Colour by object
 #
+# http://www.pymolwiki.org/index.php?title=Color_Objects
+#
 #####################################################################
+from pymol import cmd
 
 def color_obj(rainbow=0):
-        """
+    """
 AUTHOR
 
         Gareth Stockwell
@@ -19,51 +22,54 @@ USAGE
         colours used by PyMOL (in which case the 23rd object, if it exists,
         gets the same colour as the first), or are the colours of the rainbow
 
-        """
+SEE ALSO
 
-        # Process arguments
-        rainbow = int(rainbow)
+        util.color_objs()
+    """
 
-        # Get names of all PyMOL objects
-        obj_list = cmd.get_names('models')
+    # Process arguments
+    rainbow = int(rainbow)
 
-        if rainbow:
+    # Get names of all PyMOL objects
+    obj_list = cmd.get_names('objects')
 
-           print "\nColouring objects as rainbow\n"
+    if rainbow:
 
-           nobj = len(obj_list)
+        print "\nColouring objects as rainbow\n"
 
-           # Create colours starting at blue(240) to red(0), using intervals
-           # of 240/(nobj-1)
-           for j in range(nobj):
-              hsv = (240-j*240/(nobj-1), 1, 1)
-              # Convert to RGB
-              rgb = hsv_to_rgb(hsv)
-              # Define the new colour
-              cmd.set_color("col" + str(j), rgb)
-              print obj_list[j], rgb
-              # Colour the object
-              cmd.color("col" + str(j), obj_list[j])
+        nobj = len(obj_list)
 
-        else:
+        # Create colours starting at blue(240) to red(0), using intervals
+        # of 240/(nobj-1)
+        for j in range(nobj):
+            hsv = (240-j*240/(nobj-1), 1, 1)
+            # Convert to RGB
+            rgb = hsv_to_rgb(hsv)
+            # Define the new colour
+            cmd.set_color("col" + str(j), rgb)
+            print obj_list[j], rgb
+            # Colour the object
+            cmd.color("col" + str(j), obj_list[j])
 
-           print "\nColouring objects using PyMOL defined colours\n"
+    else:
 
-           # List of available colours
-           colours = ['red', 'green', 'blue', 'yellow', 'violet', 'cyan',    \
-           'salmon', 'lime', 'pink', 'slate', 'magenta', 'orange', 'marine', \
-           'olive', 'purple', 'teal', 'forest', 'firebrick', 'chocolate',    \
-           'wheat', 'white', 'grey' ]
-           ncolours = len(colours)
+        print "\nColouring objects using PyMOL defined colours\n"
 
-           # Loop over objects
-           i = 0
-           for obj in obj_list:
-              print "  ", obj, colours[i]
-              cmd.color(colours[i], obj)
-              i = i+1
-              if(i == ncolours):
-                 i = 0
+        # List of available colours
+        colours = ['red', 'green', 'blue', 'yellow', 'violet', 'cyan',    \
+        'salmon', 'lime', 'pink', 'slate', 'magenta', 'orange', 'marine', \
+        'olive', 'purple', 'teal', 'forest', 'firebrick', 'chocolate',    \
+        'wheat', 'white', 'grey' ]
+        ncolours = len(colours)
+
+        # Loop over objects
+        i = 0
+        for obj in obj_list:
+            print "  ", obj, colours[i]
+            cmd.color(colours[i], obj)
+            i = i+1
+            if(i == ncolours):
+                i = 0
 
 
 # HSV to RGB routine taken from Robert L. Campbell's color_b.py script
@@ -71,41 +77,41 @@ USAGE
 # Original algorithm from: http://www.cs.rit.edu/~ncs/color/t_convert.html
 def hsv_to_rgb(hsv):
 
-        h = float(hsv[0])
-        s = float(hsv[1])
-        v = float(hsv[2])
+    h = float(hsv[0])
+    s = float(hsv[1])
+    v = float(hsv[2])
 
-        if( s == 0 ) :
-                #achromatic (grey)
-                r = g = b = v
+    if( s == 0 ) :
+        #achromatic (grey)
+        r = g = b = v
 
+    else:
+        # sector 0 to 5
+        h = h/60.
+        i = int(h)
+        f = h - i                       # factorial part of h
+        #print h,i,f
+        p = v * ( 1 - s )
+        q = v * ( 1 - s * f )
+        t = v * ( 1 - s * ( 1 - f ) )
+
+        if i == 0:
+            (r,g,b) = (v,t,p)
+        elif i == 1:
+            (r,g,b) = (q,v,p)
+        elif i == 2:
+            (r,g,b) = (p,v,t)
+        elif i == 3:
+            (r,g,b) = (p,q,v)
+        elif i == 4:
+            (r,g,b) = (t,p,v)
+        elif i == 5:
+            (r,g,b) = (v,p,q)
         else:
-                # sector 0 to 5
-                h = h/60.
-                i = int(h)
-                f = h - i                       # factorial part of h
-                #print h,i,f
-                p = v * ( 1 - s )
-                q = v * ( 1 - s * f )
-                t = v * ( 1 - s * ( 1 - f ) )
+            (r,g,b) = (v,v,v)
+            print "error, i not equal 1-5"
 
-                if i == 0:
-                        (r,g,b) = (v,t,p)
-                elif i == 1:
-                        (r,g,b) = (q,v,p)
-                elif i == 2:
-                        (r,g,b) = (p,v,t)
-                elif i == 3:
-                        (r,g,b) = (p,q,v)
-                elif i == 4:
-                        (r,g,b) = (t,p,v)
-                elif i == 5:
-                        (r,g,b) = (v,p,q)
-                else:
-                        (r,g,b) = (v,v,v)
-                        print "error, i not equal 1-5"
-
-        return [r,g,b]
+    return [r,g,b]
 
 
 
