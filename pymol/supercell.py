9d093f53675686e9bdb858a550cfd9a39f46222a
Spencer Bliven <sbliven@ucsd.edu>
2015-06-02 12:04:26 +0200

    Adding center option to supercell

--- pymol/supercell.py
+++ pymol/supercell.py
@@ -1,14 +1,14 @@
 '''
 (c) 2010 Thomas Holder
- 
+
 PyMOL python script (load with `run supercell.py`)
 Usage: See "help supercell" and "help symexpcell"
 '''
- 
+
 from pymol import cmd, cgo, xray
 from math import cos, sin, radians, sqrt
 import numpy
- 
+
 def cellbasis(angles, edges):
 	'''
 	For the unit cell with given angles and edge lengths calculate the basis
@@ -23,42 +23,45 @@ def cellbasis(angles, edges):
 	basis[2][2] = sqrt(1 - basis[0][2]**2 - basis[1][2]**2)
 	edges.append(1.0)
 	return basis * edges # numpy.array multiplication!
- 
-def supercell(a=1, b=1, c=1, object=None, color='blue', name='supercell', withmates=1,prefix="m",transformation=None):
+
+def supercell(a=1, b=1, c=1, object=None, color='blue', name='supercell', withmates=1,prefix="m",center=0,transformation=None):
 	'''
 DESCRIPTION
- 
+
     Draw a supercell, as requested by Nicolas Bock on the pymol-users
     mailing list (Subject: [PyMOL] feature request: supercell construction
     Date: 04/12/2010 10:12:17 PM (Mon, 12 Apr 2010 14:12:17 -0600))
- 
+
 USAGE
- 
+
     supercell a, b, c [, object [, color [, name [, withmates]]]]
- 
+
 ARGUMENTS
- 
+
     a, b, c = integer: repeat cell in x,y,z direction a,b,c times
     {default: 1,1,1}
- 
+
     object = string: name of object to take cell definition from
- 
+
     color = string: color of cell {default: blue}
- 
+
     name = string: name of the cgo object to create {default: supercell}
- 
+
     withmates = bool: also create symmetry mates in displayed cells
     {default: 1}
 
     prefix = string: prefix for the symmetry mates {default: m}
 
+    center = boolean: If 1, indicates that the lattice should be centered on the
+    origin, as opposed to having the corner at the origin cell. {default: 0}
+
     transformation = list: a 16-element list giving the 4x4 transformation
     matrix, as described in get_object_matrix() {default: identity matrix}
 
 SEE ALSO
- 
+
     show cell
- 
+
     cmd
 	'''
 	if object is None:
@@ -68,7 +71,7 @@ SEE ALSO
 	sym = cmd.get_symmetry(object)
 	cell_edges = sym[0:3]
 	cell_angles = sym[3:6]
- 
+
 	basis = cellbasis(cell_angles, cell_edges)
 
 	if transformation is not None:
@@ -76,23 +79,36 @@ SEE ALSO
 	assert isinstance(basis, numpy.ndarray)
 
 	ts = list()
-	for i in range(int(a)):
-		for j in range(int(b)):
-			for k in range(int(c)):
+	a = int(a)
+	b = int(b)
+	c = int(c)
+	if int(center) == 0:
+		astart = 0
+		bstart = 0
+		cstart = 0
+	else:
+		#TODO Maybe would be more useful to center at the asymmetric unit?
+		# For now, center on the origin cell.
+		astart = (1-a)/2
+		bstart = (1-b)/2
+		cstart = (1-c)/2
+	for i in range( astart,astart+a ):
+		for j in range( bstart,bstart+b ):
+			for k in range( cstart,cstart+c ):
 				ts.append([i,j,k])
- 
+
 	obj = [
 		cgo.BEGIN,
 		cgo.LINES,
 		cgo.COLOR,
 	]
 	obj.extend(cmd.get_color_tuple(color))
- 
+
 	for t in ts:
 		# draw bounding box around cell t
 		shift = basis[0:3,0:3] * t
 		shift = shift[:,0] + shift[:,1] + shift[:,2]
- 
+
 		for i in range(3):
 			# vi is direction of the edges to draw
 			vi = basis[0:3,i]
@@ -115,49 +131,49 @@ SEE ALSO
 				obj.extend(start.tolist())
 				obj.append(cgo.VERTEX)
 				obj.extend(end.tolist())
- 
+
 		if withmates:
-			symexpcell('%s%d%d%d_' % tuple([prefix]+list(t)), object, *t,transformation=transformation)
- 
+			symexpcell('%s%d%d%d_' % (prefix,t[0]-astart,t[1]-bstart,t[2]-cstart), object, *t,transformation=transformation)
+
 	obj.append(cgo.END)
- 
+
 	cmd.delete(name)
 	cmd.load_cgo(obj, name)
- 
+
 def symexpcell(prefix='mate', object=None, a=0, b=0, c=0,transformation=None):
 	'''
 DESCRIPTION
- 
+
     Creates all symmetry-related objects for the specified object that
     occur with their bounding box center within the unit cell.
- 
+
 USAGE
- 
+
     symexpcell prefix, object, [a, b, c]
- 
+
 ARGUMENTS
- 
+
     prefix = string: prefix of new objects
- 
+
     object = string: object for which to create symmetry mates
- 
+
     a, b, c = integer: create neighboring cell {default: 0,0,0}
- 
+
     transformation = list: list of 16 floats giving the transformation matrix
     to apply to the generated symmetry mates {default: identity matrix}
 
 SEE ALSO
- 
+
     symexp, http://www.pymolwiki.org/index.php/SuperSym
 	'''
 	if object is None:
 		object = cmd.get_object_list()[0]
- 
+
 	sym = cmd.get_symmetry(object)
 	cell_edges = sym[0:3]
 	cell_angles = sym[3:6]
 	spacegroup = sym[6]
- 
+
 	basis = cellbasis(cell_angles, cell_edges)
 	basis = numpy.matrix(basis)
 
@@ -165,32 +181,33 @@ SEE ALSO
 	center = sum(numpy.array(extent)) * 0.5
 	center = numpy.matrix(center.tolist() + [1.0]).T
 	center_cell = basis.I * center
- 
+
 	extra_shift = [[float(i)] for i in (a,b,c)]
- 
+
 	i = 0
 	matrices = xray.sg_sym_to_mat_list(spacegroup)
 	for mat in matrices:
 		i += 1
- 
+
 		mat = numpy.matrix(mat)
 		shift = numpy.floor(mat * center_cell)
 		mat[0:3,3] -= shift[0:3,0]
 		mat[0:3,3] += extra_shift
- 
+
 		mat = basis * mat * basis.I
 		mat_list = list(mat.flat)
- 
+
 		name = '%s%d' % (prefix, i)
 		cmd.create(name, object)
 		cmd.transform_object(name, mat_list)
+		# Apply extra transformation afterwards
 		if transformation is not None:
 			cmd.transform_object(name, transformation)
 		cmd.color(i+1, name)
- 
+
 cmd.extend('symexpcell', symexpcell)
 cmd.extend('supercell', supercell)
- 
+
 def transformation_to_numpy(transformation):
 	if len(transformation) != 16:
 		print "Invalid transformation. Expect 16-element transformation matrix, found %d."%len(transformation)
@@ -208,3 +225,4 @@ def numpy_to_transformation(mat):
 # tab-completion of arguments
 cmd.auto_arg[3]['supercell'] = [ cmd.object_sc, 'object', '']
 
+# vim:ts=4 sw=4 noet
