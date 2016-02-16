d471ab6ef97692c108691b89d75c4656993d11e1
Spencer Bliven <sbliven@ucsd.edu>
2016-02-16 18:31:46 +0100

    Pymol: alignlattice works with selections

--- pymol/alignlattice.py
+++ pymol/alignlattice.py
@@ -22,36 +22,45 @@ USAGE
 
 ARGUMENTS
 
-    target = string: name of object to generate the first lattice. This lattice is generated to include the 
+    target = string: name of object to generate the first lattice. This lattice
+    is generated aligned to the original coordinates.
+
+    mobile = string: name of the second object. The generated lattice will
+    align to the original object.
+
     a, b, c = integer: repeat cell in x,y,z direction a,b,c times
     {default: 1,1,1}
 
-    object = string: name of object to take cell definition from
+    color1/2 = string: color of unit cell cgo objects {default: blue/red}
 
-    color = string: color of cell {default: blue}
+    name1/2 = string: name of the cgo object to create {default: supercell1/2}
 
-    name = string: name of the cgo object to create {default: supercell}
+    prefix1/2 = string: prefix for the symmetry mates {default: m/n}
 
     withmates = bool: also create symmetry mates in displayed cells
     {default: 1}
 
-    prefix = string: prefix for the symmetry mates {default: m}
-
-    center = boolean: If 1, indicates that the lattice should be centered on the
-    origin, as opposed to having the corner at the origin cell. {default: 0}
-
-    transformation = list: a 16-element list giving the 4x4 transformation
-    matrix, as described in get_object_matrix() {default: identity matrix}
-
     cutoff = int: restrict symmetry mates to within cutoff angstroms of the origin.
     Use 0 to generate all symmetry mates. {default: 0}
+
 SEE ALSO
 
     show cell
 
     cmd
 	'''
-    mobilecopy = "mobile2" #todo use unique name
+    # Check that selections contain a single object each
+    mobileobj = "(byobj (%s))" % mobile
+    targetobj = "(byobj (%s))" % target
+
+    if len(cmd.get_object_list(mobileobj)) != 1:
+        print("Expected single object for %s" % mobile)
+        return
+    if len(cmd.get_object_list(targetobj)) != 1:
+        print("Expected single object for %s" % target)
+        return
+
+    mobilecopy = "mobile2823" #todo use unique name
     # Copy the mobile unit and get its superposition with the target
     cmd.create(mobilecopy,mobile)
     initial_mat = cmd.get_object_matrix(mobilecopy)
@@ -63,9 +72,9 @@ SEE ALSO
     orig_objects = set(cmd.get_object_list())
 
     # Generate primary grid
-    supercell(a,b,c,target,color=color1,name=name1,withmates=withmates,prefix=prefix1,center=1,transformation=None,cutoff=cutoff)
+    supercell(a,b,c,targetobj,color=color1,name=name1,withmates=withmates,prefix=prefix1,center=1,transformation=None,cutoff=cutoff)
     # Generate rotated grid
-    supercell(a,b,c,mobile,color=color2,name=name2,withmates=withmates,prefix=prefix2,center=1,transformation=final_mat,cutoff=cutoff)
+    supercell(a,b,c,mobileobj,color=color2,name=name2,withmates=withmates,prefix=prefix2,center=1,transformation=final_mat,cutoff=cutoff)
 
     colored_objects1 = set(cmd.get_object_list("(%s*)"%prefix1)) - orig_objects
     colored_objects2 = set(cmd.get_object_list("(%s*)"%prefix2)) - orig_objects
